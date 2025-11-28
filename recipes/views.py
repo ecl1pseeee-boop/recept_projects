67f5187aea4edc856358acfd1a7b5da4c524e0a0
user_name <fedulov.06@mail.ru>
2025-11-28 21:23:42 +0500

    main page + recipe card

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
-# Create your views here.
+from recipes.models import Recipe
+
+
+def home(request: HttpRequest):
+    return render(request, 'home.html')
+
+def receipt_list(request: HttpRequest):
+    recipes = Recipe.objects.all()
+    return render(request, 'all_recipes.html', {'recipes': recipes})
+
+def create_recipe(request: HttpRequest):
+    pass
