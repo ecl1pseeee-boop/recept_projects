fdf92ab6427571cf0bb3aa5a828662f6051be516
ecl1pseeee-boop <maks.24156@gmail.com>
2025-12-11 23:36:06 +0500

    gitignore delete database

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from recipes.models import Recipe, Category, Ingredient
+
+admin.site.register(Recipe)
+admin.site.register(Category)
+admin.site.register(Ingredient)
 # Register your models here.
